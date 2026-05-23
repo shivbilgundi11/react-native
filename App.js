9458e16d5fb2fbe33b8fe8b8590a2687b3a8d2db
shivbilgundi11 <shivbbilgundi11@gmail.com>
2026-05-23 21:19:24 +0530

    feat: added first ever list rendering in the React Native

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { FlatList } from "react-native/types_generated/Libraries/Animated/AnimatedExports";
 
 export default function App() {
+  const DATA = [
+    { id: "1", title: "Item 1" },
+    { id: "2", title: "Item 2" },
+    { id: "3", title: "Item 3" },
+    { id: "4", title: "Item 4" },
+    { id: "5", title: "Item 5" },
+    { id: "6", title: "Item 6" },
+    { id: "7", title: "Item 7" },
+    { id: "8", title: "Item 8" },
+    { id: "9", title: "Item 9" },
+    { id: "10", title: "Item 10" },
+  ];
+
   return (
     <View style={styles.container}>
       <Text>First text on React Native Learning</Text>
@@ -14,6 +28,15 @@ export default function App() {
         Hello
       </TouchableOpacity>
       <StatusBar style="auto" />
+
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
