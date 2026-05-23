bf6fb6d0c9ca67c42d3b130c41d92d07aeef2d6d
shivbilgundi11 <shivbbilgundi11@gmail.com>
2026-05-23 21:14:55 +0530

    feat: first touchable UI added with hello message

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>First text on React Native Learning</Text>
       <Text>Welcome to learning React Native</Text>
+      <TouchableOpacity
+        onPress={() => {
+          console.log("Hello");
+        }}
+      >
+        Hello
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
